c229b969f2d9651d4aec808dd0fda6c1754d76b9
AlexaPekar <missdreambelle@gmail.com>
2018-01-11 09:38:04 +0100

    Alexa accidentally wrote an 'S' letter to the file, than deleted it

--- gittie.py
+++ gittie.py
@@ -1,11 +1,8 @@
 def introduce():
-<<<<<<< HEAD
     print("Hello, I'm Gittie!")
     print("This will produce a nice git conflict!")
-=======
     print("Hello, I'm Bence!")
     print("What's up, Alexa?")
->>>>>>> 640211466adedf4935b34d935d08d11d132d55a1
 
 def add(a, b):
     return a + b
